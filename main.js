091c5d25d603516fa28725a884bb44391cc8e1d8
Victor239A <victoraju239932@gmail.com>
2025-07-29 09:11:42 -0600

    seccion1

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 import { header } from "./componentes/header/headerComponent.js";
+import { seccion1 } from "./componentes/seccion1/seccion1Component.js";
 
 function seccion() {
     
    let seccion = document.createElement('section');
+//header
+   seccion.appendChild(header());
 
-seccion.appendChild(header());
-
-   let h1 = document.createElement('h1');
-   h1.className = "titulo";
-   h1.innerText = "Hola mundo";
-   seccion.appendChild(h1);
-   
-  let p = document.createElement('p');
-  p.innerText = "Introduccion a JavaScript"
-  seccion.appendChild(p);
-
+   //seccion 
+   seccion.appendChild(seccion1());
+  
    return seccion;
+   
 
 }
 
